9522581b44ab0458e973508ae2f1e9e6a6d72adb
pjming00006 <pjming00006@gmail.com>
2025-10-30 20:15:54 -0700

    add curated scd type 1 user table

--- dbt-analytics/athena_dbt_analytics/models/curated/user_scd1.sql
+++ dbt-analytics/athena_dbt_analytics/models/curated/user_scd1.sql
@@ -1,3 +1,18 @@
-select
-    *
-from {{ ref('user_cdc') }}
+WITH latest_user AS (
+    SELECT
+        *
+        , ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY updated_at DESC) AS rk
+    FROM {{ ref('user_cdc') }}
+)
+
+SELECT
+    user_id
+    , event_type
+    , partner_id
+    , program_id
+    , program_start_date
+    , client_name
+    , updated_at
+    , CAST(CURRENT_TIMESTAMP AS TIMESTAMP) AS loaded_at
+FROM latest_user
+WHERE rk = 1

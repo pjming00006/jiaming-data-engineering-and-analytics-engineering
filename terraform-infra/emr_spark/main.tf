02e605e164fdde9de4a370448fc4fe308b613eb8
Jiaming Pei <pjming00006@gmail.com>
2025-11-11 20:29:11 -0800

    add EMR security groups

--- terraform-infra/emr_spark/main.tf
+++ terraform-infra/emr_spark/main.tf
@@ -2,7 +2,6 @@ resource "aws_security_group" "emr_master_sg" {
   name   = "emr-master-sg"
   vpc_id = var.emr_vpc_id
 
-  # Allow outbound to all (workers, S3)
   egress {
     from_port   = 0
     to_port     = 0
@@ -15,11 +14,28 @@ resource "aws_security_group" "emr_core_sg" {
   name   = "emr-core-sg"
   vpc_id = var.emr_vpc_id
 
-  # Allow outbound to master SG
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = [aws_security_group.emr_master_sg.id]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group_rule" "core_to_master" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.emr_core_sg.id
+  security_group_id        = aws_security_group.emr_master_sg.id
+}
+
+resource "aws_security_group_rule" "master_to_core" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.emr_master_sg.id
+  security_group_id        = aws_security_group.emr_core_sg.id
 }

7b5be80aad92ac8948b6df96cff5e89bd658c99c
Jiaming Pei <pjming00006@gmail.com>
2025-12-08 16:22:55 -0800

    refactor spark trigger dag

--- airflow/dags/sample_emr.py
+++ airflow/dags/sample_emr.py
@@ -4,10 +4,9 @@ from airflow.providers.amazon.aws.operators.emr import EmrCreateJobFlowOperator
 from airflow.providers.amazon.aws.sensors.emr import EmrJobFlowSensor
 from datetime import datetime, timedelta
 
-# Replace this with your JAR path
-JAR_S3_PATH = f"s3://{Variable.get("de_etl_bucket")}/emr_packages/spark-scala-assembly-0.1.0.jar"
+JAR_S3_PATH = f"s3://{Variable.get("de_etl_bucket")}/emr_packages/{{{{ jar_name }}}}"
 
-# ----- EMR cluster configuration -----
+# EMR cluster configuration
 JOB_FLOW_OVERRIDES = {
     "Name": "single-step-emr-job",
     "ReleaseLabel": "emr-7.12.0",
@@ -17,7 +16,7 @@ JOB_FLOW_OVERRIDES = {
             {
                 "Name": "Master",
                 "InstanceRole": "MASTER",
-                "InstanceType": "r8g.xlarge",
+                "InstanceType": "r8g.xlarge", # r8g.xlarge, c5.xlarge
                 "InstanceCount": 1,
             },
         ],
@@ -38,7 +37,7 @@ JOB_FLOW_OVERRIDES = {
                 "Args": [
                     "spark-submit",
                     "--deploy-mode", "cluster",
-                    "--class", "HelloSpark", # It's required to specify an entrypoint
+                    "--class", "{{ params.entrypoint_class }}", # It's required to specify an entrypoint
                     JAR_S3_PATH
                 ],
             },
@@ -47,7 +46,7 @@ JOB_FLOW_OVERRIDES = {
     "JobFlowRole": f"arn:aws:iam::{Variable.get("aws_account_id")}:instance-profile/emr-instance-profile",
     "ServiceRole": f"arn:aws:iam::{Variable.get("aws_account_id")}:role/emr-service-role",
     "AutoTerminationPolicy": {"IdleTimeout": 60},
-    "LogUri": f"s3://{Variable.get("de_etl_bucket")}/spark-test/logs/{{ run_id }}/",
+    "LogUri": f"s3://{Variable.get("de_etl_bucket")}/spark-test/logs/{{{{ run_id }}}}/",
 }
 
 # ----- DAG -----
@@ -57,6 +56,8 @@ with DAG(
     schedule=None,
     catchup=False,
     default_args={"retries": 0},
+    params={"entrypoint_class": "HelloSpark",
+            "jar_name": "spark-scala-assembly-0.1.0.jar"}
 ):
 
     create_cluster = EmrCreateJobFlowOperator(
